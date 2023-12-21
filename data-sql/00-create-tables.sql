ed8761ce1ac4e3128aec96f3ec6a36530c2bf9ea
Douglas S. J. De Couto <decouto@alum.mit.edu>
2023-12-21 08:58:12 -0400

    Add Capital Expense Summary data

--- data-sql/00-create-tables.sql
+++ data-sql/00-create-tables.sql
@@ -68,6 +68,17 @@ create table BudgetEmploymentSummary (
         )
 ;
 
+create table CapitalExpenditureSummary (
+		BudgetBookVersion nvarchar(7)
+		, Ministry nvarchar(255)
+		, HeadNumber nvarchar(3)
+		, HeadName nvarchar(255)
+		, BudgetYear nvarchar(7)
+		, DataVersion nvarchar(8)
+		, DataValue numeric(15, 0)
+        )
+;
+
 create table MinistrySort (
 		Ministry nvarchar(255)
 		, SortOrder int
